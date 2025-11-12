d48838db0e09c07caa285516f151b6f88b194737
Talha Keskin <50806631+talhakeskin@users.noreply.github.com>
2025-11-12 22:28:03 +0300

    remove unused classes

---
+++ AghanimsInventoryApi/Controllers/V1/HeroV1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AghanimsInventoryApi.Controllers.V1;
+
+[Route("heroes")]
+[ApiController]
+public class HeroV1Controller : ControllerBase
+{
+    public HeroV1Controller()
+    {
+
+    }
+
+    [HttpGet("")]
+    public async Task<IActionResult> Test()
+    {
+        return Ok();
+    }
+}
--- AghanimsInventoryApi/Program.cs
+++ AghanimsInventoryApi/Program.cs
@@ -2,10 +2,10 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
