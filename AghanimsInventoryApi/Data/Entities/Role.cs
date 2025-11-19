87ce687f40e49ef9582c58d57ffc928955bd10c3
Talha Keskin <50806631+talhakeskin@users.noreply.github.com>
2025-11-19 18:49:55 +0300

    update prop types for role

--- AghanimsInventoryApi/Data/Entities/Role.cs
+++ AghanimsInventoryApi/Data/Entities/Role.cs
@@ -4,7 +4,7 @@ public class Role
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
-    public int Order { get; set; }
+    public byte Order { get; set; }
 }
---
+++ AghanimsInventoryApi/Migrations/20251119154851_UpdatePropTypesForRarity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AghanimsInventoryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePropTypesForRarity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Order",
+                table: "Roles",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Order",
+                table: "Roles",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+    }
+}
