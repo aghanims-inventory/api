5abe751f54a9f6c79d6b0d1845392c4f9a81c517
Talha Keskin <50806631+talhakeskin@users.noreply.github.com>
2025-11-19 19:20:02 +0300

    update attribute props

---
+++ AghanimsInventoryApi/Migrations/20251119161939_UpdateAttributeProps.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AghanimsInventoryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAttributeProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
